7c96759043a1e59123c8c7360d51754277ba632b
the-name-royce-was-taken <royce2176@gmail.com>
2026-08-06 18:06:14 +1000

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./styles/app.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+    throw new Error("Root element (#root) was not found.");
+}
+
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
