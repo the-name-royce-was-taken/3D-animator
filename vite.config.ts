6cd11caff90d399efb0fbcd50bff8038da10aa8f
the-name-royce-was-taken <royce2176@gmail.com>
2026-08-08 21:31:24 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import {
-    defineConfig,
-} from "vite";
-
-import react from
-    "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     base: "/3D-animator/",
-    plugins: [
-        react(),
-    ],
-    build: {
-        outDir: "dist",
-        assetsDir: "assets",
-        sourcemap: true,
-    },
+    plugins: [react()],
 });
