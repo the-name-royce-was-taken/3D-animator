2ce84e14cb1a734a65fa2cba91585b52f986c6bf
the-name-royce-was-taken <royce2176@gmail.com>
2026-08-09 19:28:15 +1000

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    base: "/3D-animator/",
+    plugins: [react()],
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        sourcemap: true
+    }
+});
